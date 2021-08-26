c719d5e298a4bcabf1188713e210bb2092ce0139
Fivab <83701817+Fivab@users.noreply.github.com>
2021-08-26 15:53:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+img="";
+status="";
+object=[];
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting Objects";
+}
+
+function preload() {
+    img=loadImage('dog and cat.jpg');
+}
+
+function modelLoaded() {
+    console.log("Model is initialized");
+    status=true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        objects=results;
+    }
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+
+    if(status!="") {
+        document.getElementById("status").innerHTML="Status : Objects Detected";
+        for(i=0; i<objects.length;i++) {
+        fill("#FF0000");
+        percentage=floor(objects[i].confidence*100)
+        text(objects[i].label+" "+percentage, objects[i].x+15, objects[i].y+15);
+        noFill();
+        stroke("#FF0000");
+        rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        
+        }
+}
+
+}
